391d28a82f33c2b3994e64ee9fb3de65875ee090
2002-rabia <110257843+2002-rabia@users.noreply.github.com>
2022-08-18 20:19:05 +0300

    Add files via upload

---
+++ proje.js
@@ -0,0 +1,16 @@
+
+
+var isim = prompt("Lütfen adınızı giriniz.");
+   
+   if( (isim != "") && (isim != null) ) 
+   
+   alert("Merhaba " + isim + "!"+" "+"Hoşgeldin");
+      
+   else if(isim == "") 
+   
+   alert("Boş giriş yaptınız!");
+   
+   else if(isim == null) 
+  
+   alert("Giriş yapmaktan vazgeçtiniz!");
+
